28b141f78bc667096388917a5e4a66bd7aa035a3
JawadAhbab <jawad.ahbab.turna@gmail.com>
2025-06-06 07:12:21 +0600

    🔥 Code Beautify

--- src/core/gen.ts
+++ src/core/gen.ts
@@ -1,16 +1,14 @@
+import { writeFileSync } from 'fs'
 import { join } from 'path'
 import { routine } from '../-routine'
 import { Plot } from './Plot/Plot'
-import { readFileSync, writeFileSync } from 'fs'
+import { template } from './templates/template'
 const savepath = join(process.cwd(), 'src/routine.html')
-const temppath = join(process.cwd(), 'src/core/HTML/template.html')
-const template = readFileSync(temppath).toString()
 
 console.clear()
 gen()
 function gen() {
   const plot = new Plot()
   routine(plot)
-  const html = template.replace('{$}', plot.register.createHTML())
-  writeFileSync(savepath, html)
+  writeFileSync(savepath, template('gen', plot.register.createHTML()))
 }
